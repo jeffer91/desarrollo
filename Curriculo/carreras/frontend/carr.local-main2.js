812e9718d19d3ba5dc0ef109e02ee25e66efbfc2
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-25 15:41:49 -0500

    Curriculo bloque 4: crear compatibilidad main local carreras

---
+++ Curriculo/carreras/frontend/carr.local-main2.js
@@ -0,0 +1,9 @@
+/*
+Nombre del archivo: carr.local-main2.js
+Ubicación: /Curriculo/carreras/frontend/carr.local-main2.js
+Función:
+- Mantener compatibilidad con referencias anteriores al modo local
+- Cargar el controlador principal actualizado de carreras
+*/
+
+import "./carr.main.js";
