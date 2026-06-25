ac66cbea77c3ea3573d42832eb76fd9049ed63f0
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-25 15:41:37 -0500

    Curriculo bloque 4: crear compatibilidad tabla local carreras

---
+++ Curriculo/carreras/frontend/carr.local-table.js
@@ -0,0 +1,9 @@
+/*
+Nombre del archivo: carr.local-table.js
+Ubicación: /Curriculo/carreras/frontend/carr.local-table.js
+Función:
+- Mantener compatibilidad con referencias anteriores
+- Reutilizar la tabla principal actualizada de carreras
+*/
+
+export { carrCrearTablaCarreras } from "./carr.tabla.js";
